f3994fd66993361bb4fbc3d19d9f344d1eb672bc
divramod <divramod@gmail.com>
2020-04-09 18:17:52 +0200

    [envrc] add project envrc for development

--- scripts/bokeh.sh
+++ scripts/bokeh.sh
@@ -1,10 +1,11 @@
-#!/bin/bash
+#!/usr/bin/env sh 
+: "${PATH_BASE:="$(git rev-parse --show-toplevel)"}"
 
 export HTTP_PROXY=
 export HTTPS_PROXY=
 export http_proxy=
 export https_proxy=
-cd ..
 # python restservice.py occupancydemo &
+cd "${PATH_BASE}" 
 bokeh serve bokeh_web --allow-websocket-origin="*" --port 5006 --args http://127.0.0.1:6001/ root.visualization.workbench
 # bokeh serve bokeh_web --allow-websocket-origin="*" --port 5010 --args http://127.0.0.1:6001/ root.visualization.selfservice0 &
