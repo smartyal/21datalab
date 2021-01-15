f65dd73fb198c2a38cd45c1e4f3a071254d23fa5
smartyal <albert_krohn@yahoo.de>
2021-01-15 13:37:26 +0100

    add periodic alarm and save model for auto saving the model per timer the alarm clock (compared to the periodic timer) is threaded and can be interrupted before timeout

--- plugins/system.py
+++ plugins/system.py
@@ -2,6 +2,8 @@ import datetime
 import time
 import json
 import copy
+import dates
+from model import getRandomId
 
 __functioncontrolfolder = {
     "name":"control","type":"folder","children":[
@@ -16,6 +18,20 @@ __functioncontrolfolder = {
         {"name":"executionType","type":"const","value":"async"}             # one of ["sync", "async"] sync: is executed on the main server thread, async: in separate thread
     ]
 }
+__functioncontrolfolder_threaded = {
+    "name":"control","type":"folder","children":[
+        {"name":"status","type":"variable","value":"idle"},                 # one of ["finished","running"]
+        {"name":"progress","type":"variable","value":0},                    # a value between 0 and 1 ( for showing progress in the ui
+        {"name":"result","type":"variable","value":"ok"},                   # of ["ok","error","pending" or a last error message]
+        {"name":"signal","type":"variable","value":"nosignal"},             # of ["nosignal","interrupt"]
+        {"name":"executionCounter","type":"variable","value":0},            # a counter which is increased on each execution of this function
+        {"name":"lastExecutionDuration", "type": "variable", "value": 0},   #in float seconds
+        {"name":"lastStartTime","type":"variable","value":"not yet"},       # in isoforma-string
+        {"name":"executionCounter","type":"variable","value":0},            # increases on each SUCESSFUL completion
+        {"name":"executionType","type":"const","value":"threaded"}             # one of ["sync", "async"] sync: is executed on the main server thread, async: in separate thread
+    ]
+}
+
 """
 systemFolder = {
     {"name":"system","type":"folder","children":[
@@ -110,6 +126,22 @@ periodicTimer = {
     ]
 }
 
+
+alarmClock = {
+    "name":"alarmClock",
+    "type":"function",
+    "functionPointer":"system.alarm_clock",
+    "autoReload":False,
+    "children": [
+        {"name": "target", "type": "referencer"},
+        {"name": "isRunning","type":"variable","value":False}, #show if it is running or note
+        {"name": "periodSeconds", "type": "const","value":24*60*60},
+        {"name": "nextTimeout","type":"const","value":"2021-01-15T08:00+02:00"},#first alarm in iso time
+        {"name": "executionCounter", "type": "variable", "value": 0},
+        __functioncontrolfolder_threaded,
+    ]
+}
+
 counter = {
     "name":"counter",
     "type":"function",
@@ -140,8 +172,17 @@ observer = {
 }
 
 
-
-
+saveModel = {
+    "name":"saveModel",
+    "type":"function",
+    "functionPointer":"system.save_model",              #filename.functionname
+    "autoReload":False,                                 #set this to true to reload the module on each execution
+    "children":[
+        {"name":"autoName","type":"const","value":False},   #if autoName is on, we will use a new name each time
+        {"name":"name","type":"variable","value":None},
+        __functioncontrolfolder,
+    ]
+}
 
 
 
@@ -180,6 +221,39 @@ def periodic_timer(functionNode):
         time.sleep(sleep_time)
     return True
 
+def alarm_clock(functionNode):
+    # this function never ends until we get the signal "stop"
+    # it works on an own thread
+    signalNode = functionNode.get_child("control").get_child("signal")
+    logger = functionNode.get_logger()
+
+    functionNode.get_child("isRunning").set_value(True)
+    while True:
+        if signalNode.get_value() == "stop":
+            signalNode.set_value("nosignal")
+            break
+
+        timeout =  dates.date2secs(functionNode.get_child("nextTimeout").get_value())
+        now = time.time()
+        if now>timeout:
+            #set the next timeout
+            period = functionNode.get_child("periodSeconds").get_value()
+            timeout = timeout + period
+            functionNode.get_child("nextTimeout").set_value(dates.epochToIsoString(timeout))
+
+
+            for node in functionNode.get_child("target").get_leaves():
+                logger.debug(f"alarm_clock executes",node.get_name())
+                node.execute()
+
+            counterNode = functionNode.get_child("executionCounter")
+            counterNode.set_value(counterNode.get_value()+1)
+
+        time.sleep(5) #only sleep short to be able to break the loop with the signal
+
+    functionNode.get_child("isRunning").set_value(False)
+    return True
+
 
 
 def observer_function(functionNode):
@@ -209,4 +283,27 @@ def observer_function(functionNode):
                 node.execute()
             statusNode.set_value(copy.deepcopy(currentStatus))
     return True
+
+
+def save_model(functionNode):
+    logger = functionNode.get_logger()
+    model = functionNode.get_model()
+
+    if functionNode.get_child("autoName").get_value()==True:
+        fileName = model.currentModelName
+        if len(model.currentModelName)>9 and model.currentModelName[-9] == "_":
+            fileName = fileName[:-9]
+        fileName = fileName+ "_" + getRandomId()
+        functionNode.get_child("name").set_value(fileName)
+    else:
+        if functionNode.get_child("name").get_value()==None:
+            functionNode.get_child("name").set_value(model.currentModelName)
+
+    #now save it
+    fileName =  functionNode.get_child("name").get_value()
+    logger.debug(f"saving the model as {fileName}")
+    model.save(fileName)
+    return True
+
+
 
